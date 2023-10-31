b67e645cb573be35608212520a1c5f5f91e8bbc3
Oleksii S <seleznyov25@gmail.com>
2023-11-01 01:37:12 +0200

    New notes + task

--- js/index.js
+++ js/index.js
@@ -1,19 +1,40 @@
-const numberOfFilms = +prompt('Скільки фільмів ви вже перегляули?', '');
+// const numberOfFilms = +prompt('Скільки фільмів ви вже перегляули?', '');
 
-const personalMovieDB = {
-  'count': numberOfFilms,
-  'movies': {},
-  'actors': {},
-  'genres': [],
-  'privat': false
-};
+// const personalMovieDB = {
+//   'count': numberOfFilms,
+//   'movies': {},
+//   'actors': {},
+//   'genres': [],
+//   'privat': false
+// };
 
-const a = prompt('Останній фільм, який ви дивились?', ''),
-      b = prompt('Як би ви його оцінили?', ''),
-      c = prompt('Останній фільм, який ви дивились?', ''),
-      d = prompt('Як би ви його оцінили?', '');
+// const a = prompt('Останній фільм, який ви дивились?', ''),
+//       b = prompt('Як би ви його оцінили?', ''),
+//       c = prompt('Останній фільм, який ви дивились?', ''),
+//       d = prompt('Як би ви його оцінили?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+
+console.log(NaN || 2 || undefined);
+
+console.log( NaN || 2 || undefined ); // 2
+
+console.log( NaN && 2 && undefined ); // Nan
+
+console.log( 1 && 2 && 3 ); // 3
+
+console.log( !1 && 2 || !3 ); 
+
+console.log( 25 || null && !3 );
+
+console.log( NaN || null || !3 || undefined || 5);
+
+console.log( NaN || null && !3 && undefined || 5);
+
+console.log( 5 === 5 && 3 > 1 || 5);
+
+
