ea70821fe3de61425ad66faeec950266a2e7eb05
Oleksii S <seleznyov25@gmail.com>
2023-11-02 19:55:58 +0200

    New tasks and Practice #2

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+
+
+// #region Practice 1
 const numberOfFilms = +prompt('Скільки фільмів ви вже перегляули?', '');
 
 const personalMovieDB = {
@@ -8,12 +11,55 @@ const personalMovieDB = {
   'privat': false
 };
 
-const a = prompt('Останній фільм, який ви дивились?', ''),
-      b = prompt('Як би ви його оцінили?', ''),
-      c = prompt('Останній фільм, який ви дивились?', ''),
-      d = prompt('Як би ви його оцінили?', '');
+// #region перше рішення
+// const a = prompt('Останній фільм, який ви дивились?', ''),
+//       b = prompt('Як би ви його оцінили?', ''),
+//       c = prompt('Останній фільм, який ви дивились?', ''),
+//       d = prompt('Як би ви його оцінили?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+// #endregion
+
+// Варіант цикла 1
+// start: for (let i = 0; i < 2; i++) {
+//   const q1 = prompt('Останній фільм, який ви дивились?', ''),
+//         q2 = prompt('Як би ви його оцінили?', '');
+
+//   if (q1 === null || q2 === null || q1.length < 1 || q2.length < 1 || q1.length > 50) {
+//     --i;
+//     continue start;
+//   };
+
+//   personalMovieDB.movies[q1] = q2; 
+// }
+
+// Варіант цикла 2
+for (let i = 0; i < 2; i++) {
+  const q1 = prompt('Останній фільм, який ви дивились?', ''),
+        q2 = prompt('Як би ви його оцінили?', '');
+
+  if (q1 != null && q2 != null && q1 != '' && q2 != "" && q1.length < 50) {
+    personalMovieDB.movies[q1] = q2; 
+  } else {
+    i--;
+  }
+}
+
+
+if (personalMovieDB.count > 0 && personalMovieDB.count < 10) {
+  console.log('Переглянуто досить мало фільмів');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('А ви класний глядача');
+} else if (personalMovieDB.count >= 30) {
+  console.log('Да ви кіноман');
+} else {
+  console.log('Ой, щось не так... Чи ви не дивились фільми?');
+}
 
 console.log(personalMovieDB);
+
+// #endregion
+
+// #region practice 2
+
--- js/sub-tasks.js
+++ js/sub-tasks.js
@@ -1,13 +1,97 @@
-console.log(NaN || 2 || undefined); // 2
-console.log( NaN || 2 || undefined ); // 2
-console.log( NaN && 2 && undefined ); // Nan
-console.log( 1 && 2 && 3 ); // 3
-console.log( !1 && 2 || !3 ); // false
-console.log( 25 || null && !3 ); //25
-console.log( NaN || null || !3 || undefined || 5); // 5
-console.log( NaN || null && !3 && undefined || 5); // 5
-console.log( 5 === 5 && 3 > 1 || 5); // true
+// #region subtask 3
 
+// #region subtask 2**
+
+// Задача із зірочкою:
+// 1. Заполните новый массив (result) числами из старого (arr). 
+// Количество элементов в массиве можно получить как arr.length, 
+// а к элементам обращаемся все так же: arr[0], arr[1] и тд.
+// Должен получиться точно такой же массив
+
+function firstTask() {
+  const arr = [3, 5, 8, 16, 20, 23, 50];
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i]);
+    // resut[i] = arr[i]; - другий варіант запису
+  }
+
+  console.log(result);
+  return result;
+}
+
+// 2. Измените данный массив так, чтобы все числа были увеличены в 2 раза, 
+// а если попадается строка - то к ней было добавлено " - done".
+// Для определения типа данных используйте typeof();
+// Должно получиться: [ 10, 20, 'Shopping - done', 40, 'Homework - done' ]
+
+function secondTask() {
+  const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+ 
+  for (let i = 0; i < data.length; i++) {
+    if (typeof(data[i]) === 'number') {
+      data[i] += data[i];
+    } else if (typeof(data[i]) === 'string') {
+      data[i] += ' - done';
+      // data[i] = `$data[i] - done`; - другий варіант запису
+    }
+  }
+  console.log(data);
+  return data;
+}
+
+
+// 3. Разверните массив data наоборот при помощи цикла и запишите данные в массив result.
+// Должно получиться: [ 'Homework', 20, 'Shopping', 10, 5 ]
+
+function thirdTask() {
+  const data = [5, 10, 'Shopping', 20, 'Homework'];
+  const result = [];
+
+  for (let i = 1; i <= data.length; i++) {
+    result.push(data[data.length - i]);
+    // result[i-1] = data[data.length - i] - Другий варіант запису
+  }
+
+  console.log(result);
+  return result;
+}
+
+// -------------------
+
+const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) {
+  for (let j = 5; j > i; j--) {
+    result += ' ';
+  }
+  for (let k = 0; k <= i * 2; k += 2) {
+    result += '*';
+    if (k === 0) continue;
+    result += '*';
+  }
+  result += '\n';
+}
+
+// Варіант 2:
+for (let i = 0; i <= lines; i++) {
+  for (let j = 0; j < lines - i; j++) {
+    result += " ";
+  }
+  for (let j = 0; j < 2 * i + 1; j += 2) {
+    result += '*';
+  }
+  result += '\n';
+}
+
+console.log(result);
+
+// #endregion
+
+// #region example
 for (let i = 0; i < 3; i++) {
   console.log(`First level: ${i}`)
 
@@ -20,66 +104,16 @@ for (let i = 0; i < 3; i++) {
     }
   }
 }
+// #endregion
 
-// Задача із зірочкою:
-// 1. Заполните новый массив (result) числами из старого (arr). 
-// Количество элементов в массиве можно получить как arr.length, 
-// а к элементам обращаемся все так же: arr[0], arr[1] и тд.
-// Должен получиться точно такой же массив
-
-function firstTask() {
-  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-  const arr = [3, 5, 8, 16, 20, 23, 50];
-  const result = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    result.push(arr[i]);
-  }
-  console.log(result);
-  
-  // Не трогаем
-  return result;
-}
-
-// 2. Измените данный массив так, чтобы все числа были увеличены в 2 раза, 
-// а если попадается строка - то к ней было добавлено " - done".
-// Для определения типа данных используйте typeof();
-// Должно получиться: [ 10, 20, 'Shopping - done', 40, 'Homework - done' ]
-
-function secondTask() {
-  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-  const data = [5, 10, 'Shopping', 20, 'Homework'];
-
-  // Пишем решение вот тут
-  for (let i = 0; i < data.length; i++) {
-    if (typeof(data[i]) === 'number') {
-      data[i] += data[i];
-    } else {
-      data[i] += ' - done';
-    }
-  }
-  console.log(data);
-  
-  // Не трогаем
-  return data;
-}
-
-
-// 3. Разверните массив data наоборот при помощи цикла и запишите данные в массив result.
-// Должно получиться: [ 'Homework', 20, 'Shopping', 10, 5 ]
-
-function thirdTask() {
-  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-  const data = [5, 10, 'Shopping', 20, 'Homework'];
-  const result = [];
-
-  // Пишем решение вот тут
-  for (let i = 1; i <= data.length; i++) {
-    result.push(data[data.length - i]);
-  }
-  console.log(result);
-  
-  
-  // Не трогаем
-  return result;
-}
+// #region subtask 1
+console.log(NaN || 2 || undefined); // 2
+console.log( NaN || 2 || undefined ); // 2
+console.log( NaN && 2 && undefined ); // Nan
+console.log( 1 && 2 && 3 ); // 3
+console.log( !1 && 2 || !3 ); // false
+console.log( 25 || null && !3 ); //25
+console.log( NaN || null || !3 || undefined || 5); // 5
+console.log( NaN || null && !3 && undefined || 5); // 5
+console.log( 5 === 5 && 3 > 1 || 5); // true
+// #endregion
