8bd83b8cc27d573a00b91821b3a914c68d58aa03
Oleksii S <seleznyov25@gmail.com>
2023-11-22 01:02:01 +0200

    New notes ...47

--- js/script.js
+++ js/script.js
@@ -23,15 +23,43 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
+
 const adv = document.querySelectorAll('.promo__adv img'),
       bg = document.querySelector('.promo__bg'),
-      genre = bg.querySelector('.promo__genre');
-      
+      genre = bg.querySelector('.promo__genre'),
+      list = document.querySelector('.promo__interactive-list'),
+      myMovies = document.querySelectorAll('.promo__interactive-item');
 
 adv.forEach(item => {
     item.remove();
 });
-
+    
 genre.textContent = 'драма';
-
+    
 bg.style.backgroundImage = 'url("img/bg.jpg")';
+
+// Мій варіант
+// myMovies.forEach((item, i) => {
+//     item.remove();
+// });
+
+let {movies} = movieDB;
+movies.sort();
+
+list.innerHTML = '';
+
+// Мій варіант - багато переборів, але правильний
+// for (let i = 0; i < movies.length; i++) {
+//     let li = document.createElement('li');
+//     li.className = 'promo__interactive-item';
+//     li.innerHTML = `${i+1}. ${movies[i]} <div class="delete"></div>`;
+//     list.append(li);
+// }
+
+movies.forEach((item, i) => {
+    list.innerHTML += `
+        <li class="promo__interactive-item">${i+1}. ${item}
+            <div class="delete"></div>
+        </li>
+    `;
+});
