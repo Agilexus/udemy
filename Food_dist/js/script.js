a61d4b4238acc59464a2d023fb9cfeee11f3d85a
Oleksii S <seleznyov25@gmail.com>
2023-12-19 21:44:38 +0200

    Final Chapter 4

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -6,7 +6,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // #region tabs
   function hideTabContent() {
     tabsContent.forEach(item => {
-      // item.style.display = 'none'; - ілайн стилі
+      // item.style.display = 'none'; - інлайн стилі
       item.classList.add('hide');
       item.classList.remove('show', 'fade');
     });
@@ -39,6 +39,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   });
   // #endregion
+
 
   // #region timer
   /*
@@ -126,12 +127,178 @@ window.addEventListener('DOMContentLoaded', () => {
   setClock('.timer', deadline);
 
   // #endregion
+ 
 
-  // #region modal
+ // #region modal
   const modalTrigger = document.querySelectorAll('[data-modal]'),
         modal = document.querySelector('.modal'),
         modalCloseBtn = document.querySelector('[data-close]');
 
+  /* Просте рішення з інлайн стилями
+  modalTrigger.forEach(btn => {
+    btn.addEventListener('click', () => {
+      modal.style.display = 'block';
+    });
+  });
+
+  modalCloseBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
+  */
+
+  function openModal() {
+    modal.classList.add('show'); // заміть add і remove можна використовувати modal.classList.toggle('show');
+    modal.classList.remove('hide');
+
+    document.body.style.overflow = 'hidden'; // сторінка під вікном не буде скролитись
+    
+    clearInterval(modalTimerId);
+  }
+
+  modalTrigger.forEach(btn => {
+    btn.addEventListener('click', openModal);
+  });
+
+  function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+
+    document.body.style.overflow = ''; // залишаємо пусті '' і браузер сам вирішить що підставити по дефолту
+  }
+
+  modalCloseBtn.addEventListener('click', closeModal);
+
+  // Закрити вікно по кліку за межами вікна
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  // Закриваємо вікно по натисканню на esc
+  document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape' && modal.classList.contains('show')) { // Щоб дізнатись інші e.code просто загуглити event.code
+      closeModal();
+    }
+  });
+
+  // Відкриваємо вікно через певний проміжок часу
+  const modalTimerId = setTimeout(openModal, 10000);
+
+  // Відкриваємо вікно, коли користувач доскроли до кінця сторінки
+  function showModalByScroll() {
+    if (window.scrollY + document.documentElement.clientHeight >=  document.documentElement.scrollHeight -1) {
+      openModal();
+      removeEventListener('scroll', showModalByScroll); // для видалення, потрібно точно повторити, що ми призначали при створенні слухача
+    }
+  }
+
+  window.addEventListener('scroll', showModalByScroll);
+
+  /* Розбір відкриття вікна в кінці сторінки
+  window.pageYOffset - pageYOffset властивість window, яка повертає кількість 
+  пікселів, які прокурутив користувач.
   
- 
+  window.scrollY це те саме що і window.pageYOffset, але другий варіант 
+  застарів
+  
+  document.documentElement.clientHeight - clientHeight властивість Element, 
+  яка повертає висоту елементу (в даному випадку, повертає всю висоту 
+  браузера, яка вважається областю сайту)
+  
+  document.documentElement.scrollHeight - висота всього сайту, є 
+  обмежння з якими слід ознайомитись: https://learn.javascript.ru/size-and-scroll-window#shirina-vysota-dokumenta
+  
+
+  Додаткова інформація:
+      В наш window.addEventListener('scroll', func) - можна додати 3-й аргумент
+      наприклад window.addEventListener('scroll', func, {once: true}).
+      В нашому випадку він не спрацює, так як наш івент спрацьовує при 
+      кожному скролі і відповідно вкінці сторінки вже не спрацює.
+  */
+
+  // #endregion
+
+
+  // #region Клас-карточка
+  class ItemCard {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.classes = classes;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 38;
+      this.changeToUAH();
+    }
+
+    // метод для конвертації валюти в грн
+    changeToUAH() {
+      this.price = this.price * this.transfer;
+    }
+
+    // метод створення картки render()
+    render() {
+      const element = document.createElement('div');
+
+      if (this.classes.length === 0) {
+        this.element = 'menu__item';
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
+      
+      element.innerHTML = `
+        <img src=${this.src} alt=${this.alt}>
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+        </div>
+      `;
+      this.parent.append(element);
+    }
+  }
+
+  const menuBlock = document.querySelector('.menu__field').firstElementChild;
+  menuBlock.innerHTML = '';
+
+  new ItemCard(
+    "img/tabs/vegy.jpg", 
+    "vegy", 
+    'Меню "Фитнес"', 
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    6,
+    ".menu .container"
+  
+  ).render(); // тут клас записується через if в методі render
+
+  new ItemCard(
+    "img/tabs/elite.jpg", 
+    "elite", 
+    'Меню “Премиум”', 
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    15,
+    ".menu .container",
+    'menu__item'
+  ).render();
+
+  new ItemCard(
+    "img/tabs/post.jpg" , 
+    "post", 
+    'Меню "Постное"', 
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    12,
+    ".menu .container",
+    'menu__item'
+  ).render();
+
+  // #endregion
+
+
+  
 });
