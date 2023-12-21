85baba374cf04fb717f725618afdcdebd75d37fd
Oleksii S. <seleznyov25@gmail.com>
2023-12-22 00:43:27 +0200

    checkpoint

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 window.addEventListener('DOMContentLoaded', () => {
   const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
